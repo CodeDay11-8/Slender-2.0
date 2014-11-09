d59877638181117ca95bfa9bb62d82e3c4b767fe
AmeySV <ameyvaze1@gmail.com>
2014-11-09 08:42:25 -0800

    Slender AI update

---
+++ Assets/StartScreenScript1.js
@@ -0,0 +1,31 @@
+﻿var isQuit=false;
+
+function OnMouseEnter()
+{
+	renderer.material.color = Color.red;
+}
+
+function OnMouseExit()
+{
+	renderer.material.color = Color.white;
+}
+
+function OnMouseUp()
+{
+	if (isQuit==true)
+	{
+		Application.Quit();
+	}
+	else
+	{
+		Application.LoadGame();
+	}
+}
+
+function Update()
+{
+	if (Input.GetKey(KeyCode.Escape))
+	{
+		Application.Quit();
+	}
+}
